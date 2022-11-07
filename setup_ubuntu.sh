c3fc1a33bcea67ab76ee55e59b22540a03c1fd5d
RaphaelBecker <raphael.becker.private@outlook.de>
2022-11-07 19:57:51 +0100

    added ubuntu setup

--- setup_ubuntu.sh
+++ setup_ubuntu.sh
@@ -4,17 +4,15 @@ if [ -d "$DIR" ]; then
   echo "Virtual Environment already exists: ${DIR}..."
 else
   echo  "Installing python 3.9.7..."
-  apt install python3
-  ./configure --enable-optimizations
-  make altinstall
-  python3 --version
-  python3 -m pip install --upgrade pip
+  apt install python3.9
+  apt install python3.9-venv
   echo "Creating virtual environment..."
-  python3 -m venv venv
+  python3.9 -m venv venv
   echo "Activate virtual environment ${DIR}..."
   source venv/bin/activate
+  python3.9 --version
   echo "Upgrade pip..."
-  python3 -m pip install --upgrade pip
+  python3.9 -m pip install --upgrade pip
   echo "Installing requirements.txt..."
   pip3 install -r requirements.txt
 fi
